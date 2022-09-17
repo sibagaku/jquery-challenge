25609489c9918d58b3cc394b3a19849bf20120d2
EC2 Default User <ec2-user@ip-172-31-6-5.ap-northeast-1.compute.internal>
2022-09-17 06:19:05 +0000

    updata practice8

---
+++ practice8/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition: "fade",
+  speed: 1000,
+  easing: "easeOutQuart",
+  navType: "bubble",
+  childrenElementType: "div",
+  arrows: true,
+  autoPlay: true,
+  autoPlayDuration: 4000,
+  keyboardOnAlways: true,
+  hidePrevious: false,
+});
