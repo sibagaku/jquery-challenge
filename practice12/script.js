1b2ef8ef4d76821f9336d2584f009b4a5eb5193f
EC2 Default User <ec2-user@ip-172-31-6-5.ap-northeast-1.compute.internal>
2022-09-18 13:17:31 +0000

    updata practice12

--- practice12/script.js
+++ practice12/script.js
@@ -2,11 +2,12 @@ const API_KEY = "5b5278212ca52a652052353057bf1c53"
 
 $(function(){
   $("#btn").on("click", function(){
+    //入力された都市名でAPIに天気情報をリクエスト
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
-      dataType : "jsonp",
-    }).done(function (data){
-      //通信の成功
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $("#cityname").val() + "&units=metric&appid=" + API_KEY,
+      dataType: "jsonp",
+    }).done(function(data){
+      //通信成功（WEB_APIを正常に呼び出せた場合）
       $("#place").text(data.name);
       $("#temp_max").text(data.main.temp_max);
       $("#temp_min").text(data.main.temp_min);
@@ -15,8 +16,8 @@ $(function(){
       $("#weather").text(data.weather[0].main);
       $("img").attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
       $("img").attr("alt",data.weather[0].main);
-    }).fail(function (data){
-      //通信の失敗
+    }).fail(function(data){
+      //通信失敗
       alert("通信に失敗しました。");
     });
   });
